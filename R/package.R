d3b487a61d32e3eea28d2c80f64bcb3acce7ab52
rCarto <timothee.giraud@cnrs.fr>
2022-01-14 11:52:01 +0100

    docs: add documentation entry on package

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' @title Package description
+#' @name fisheye-package
+#' @description
+#' Base maps are transformed to focus on a specific location using an
+#' azimuthal logarithmic distance transformation.
+#' @references Hägerstrand, T. (1957). Migration and Area: A Survey of a Sample
+#' of Swedish Migration Fields and Hypothetical Considerations of their Genesis.
+#' Lund Studies in Geography, Series B, Human Geography, Department of
+#' Geography, University of Lund, Lund.
+#' @docType package
+NULL
